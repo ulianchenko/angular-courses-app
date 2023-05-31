4678cd8cb770ab65cd818d70b7be906bb7a40da0
ulianchenko <ulianchenko@gmail.com>
2023-06-01 02:27:13 +0300

    task-1 was added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LogoComponent } from './logo/logo.component';
+import { CoursesPageComponent } from './courses-page/courses-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent,
+    CoursesPageComponent
   ],
   imports: [
     BrowserModule,
