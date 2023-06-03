e4ca41cc42ad997bf0b11715e32310d8cd48dea3
ulianchenko <ulianchenko@gmail.com>
2023-06-03 14:47:08 +0300

    some changes in project structure was made after review

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
-import { LogoComponent } from './components/header/logo/logo.component';
-import { CoursesPageComponent } from './pages/courses-page/courses-page.component';
-import { CourseSearchComponent } from './pages/courses-page/courses-search/course-search.component';
-import { CoursesListComponent } from './pages/courses-page/courses-list/courses-list.component';
-import { CourseItemComponent } from './pages/courses-page/course-item/course-item.component';
-import { ButtonComponent } from './shared/button/button.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { BreadcrumbsComponent } from './core/components/breadcrumbs/breadcrumbs.component';
+import { LogoComponent } from './core/components/header/logo/logo.component';
+import { CoursesPageComponent } from './courses/pages/courses-page/courses-page.component';
+import { CourseSearchComponent } from './courses/components/courses-search/course-search.component';
+import { CoursesListComponent } from './courses/components/courses-list/courses-list.component';
+import { CourseItemComponent } from './courses/components/course-item/course-item.component';
+import { ButtonComponent } from './shared/components/button/button.component';
 
 @NgModule({
   declarations: [
